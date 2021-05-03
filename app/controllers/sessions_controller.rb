c5822a9fd8394f88ded569b1e757843e5480b5a4
sergey <sbarchshevskiy@gmail.com>
2021-05-03 13:55:35 -0400

    add auth routes and logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+
+  include CurrentUserConcern
+
   def create
     user = User
     .find_by(email: params["user"]["email"])
@@ -17,4 +20,23 @@ class SessionsController < ApplicationController
       }
     end
   end 
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
 end
