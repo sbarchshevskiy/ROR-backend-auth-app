ca2cef9fd67d3dd2b3e25768df76d93e63581528
sergey <sbarchshevskiy@gmail.com>
2021-05-04 15:08:47 -0400

    reinitiate react-redux

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_rails_project", domain: "https://rails-project.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_rails_project", domain: "https://rails-project.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
