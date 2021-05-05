fe174e5fdeed225931c0fd10428fd42e1be00fb3
sergey <sbarchshevskiy@gmail.com>
2021-05-05 16:04:40 -0400

    create api call to login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from "./components/Main";
 import Nav from "./components/Nav";
+import axios from 'axios';
 
 
 export default class App extends Component{
@@ -14,6 +15,39 @@ export default class App extends Component{
     };
 
     this.allowLogin = this.allowLogin.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  validateCurrentSession() {
+    axios.get("http://localhost:3001/logged_in",
+      {withCredentials: true}
+      ).then(res => {
+        if (res.data.logged_in && this.state.sessionStatus === "signed out")
+          this.setState({
+            sessionStatus: "signed in",
+            user: res.data.user
+          })
+      else if (!res.data.logged_in && this.state.sessionStatus === "signed in") {
+          this.state({
+            sessionStatus: "signed out",
+            user: {}
+          })
+        }
+    }).catch(err => {
+      console.log("error login", err)
+    })
+  }
+
+  componentDidMount() {
+    this.validateCurrentSession()
+  }
+
+  logout(){
+    this.setState({
+      sessionStatus: "signed out",
+      user: {}
+    })
+    console.log('state', this.state)
   }
 
   allowLogin(data) {
@@ -21,6 +55,7 @@ export default class App extends Component{
       sessionStatus: "signed in",
       user: data.user
     })
+    console.log('user data',data.user)
   }
 
   render() {
@@ -31,14 +66,22 @@ export default class App extends Component{
             exact
             path={"/"}
             render={props => (
-              <Main {...props} allowLogin={this.allowLogin} sessionStatus={this.state.sessionStatus} />
+              <Main
+                {...props}
+                allowLogin={this.allowLogin}
+                logout={this.logout}
+                sessionStatus={this.state.sessionStatus}
+              />
             )}
           />
           <Route
             exact
             path={"/nav"}
             render={props => (
-              <Nav {...props}  sessionStatus={this.state.sessionStatus}/>
+              <Nav {...props}
+                   allowLogin={this.allowLogin}
+                   logout={this.logout}
+                   sessionStatus={this.state.sessionStatus}/>
             )}
           />
         </Switch>
